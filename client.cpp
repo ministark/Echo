d03056dae08ca6dfc74b6723f2a6559f72c937cb
HARSH DEPAL <harshdepal@cse.iitb.ac.in>
2017-04-26 23:02:41 +0530

    fixed cursor position

--- client.cpp
+++ client.cpp
@@ -5,40 +5,35 @@
 #include <ncurses.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <map>
 using namespace std;
 #define ENTER_KEY 13
 struct Message
 {
 	string from,to,timestamp,data;
 };
-struct Notification
-{
-	int number_online,unread_messages;
-};
 struct User
 {
 	string name,status;
-};
-struct Chat
-{
-	User user;
 	vector <Message> message_list;
 };
-struct Home
-{
-	vector <User> user_list;
-};
+
 struct UI
 {
 	int type,cursor_x,cursor_y;
 	bool update,exit_program;
-	Notification notification;
+	map<string,User> user_list;
+	int number_online,unread_messages;
+	User *curr_chat_window; //The person whom we are talking with
 	char display[24][80];
 	UI()
 	{
+		curr_chat_window = NULL;
 		type = 0;
 		update = true;
 		exit_program = false;
+		number_online = 1;
+		unread_messages = 0;
 		load_ui();
 	}
 	void load_ui()
@@ -49,7 +44,7 @@ struct UI
 		if (type == 0)
 		{
 			template_name = "ui/ui_login.txt";
-			cursor_x = 14;
+			cursor_x = 13;
 			cursor_y = 39;
 		}
 		template_file.open(template_name);
@@ -67,8 +62,6 @@ struct UI
 			display[x][i+y] = s[i];
 	}
 };
-
-
 void RefreshDisplay(char display[][80])
 {
 	for (int i=0; i<24; i++)
@@ -87,6 +80,23 @@ void *Display(void *thread_arg)
 	{
 		if (ui->update)
 		{
+			if (ui->type == 0) // Login
+			{
+
+			}
+			else if (ui->type == 1) // Home 
+			{
+				//ui->edit_display(2,40,"Active Echo : " + string(itoa(ui->number_online)));
+				//ui->edit_display(2,60,"Unread Echo : " + string(itoa(ui->unread_messages)));
+
+
+				// Display number of online-user by iterating over map
+			}
+			else if (ui->type == 2) // Chat Window
+			{
+				// Check Notification Bar
+				// Process messages from user->messages and paste it in display
+			}
 			RefreshDisplay(ui->display);
 			ui->update = false;
 		}
@@ -97,7 +107,7 @@ bool authenticate(string username,string password)
 {
 	return false;
 }	
-void ProcessInput(string command)
+void ProcessInput(UI *ui)
 {
 
 	return;
@@ -149,7 +159,7 @@ void *InputHandler(void *thread_arg)
 			{
 				if (username_read)
 				{
-					ui->cursor_x = 13;
+					ui->cursor_x = 15;
 					ui->cursor_y = 39;
 				}
 				else if (password_read)
@@ -174,30 +184,36 @@ void *InputHandler(void *thread_arg)
 				}
 			}
 		}
-		//Home Screen
+		//Home Screen & Chat Windo
+		else if (ui->type == 1)
+		{
+
+		}
 		else
 		{
-			char ch = getch();
-			if (command_start and ch != 13)
+			if (ch != ENTER_KEY)
 			{	
-				ui->display[23][position++] = ch;	
-				ui->update = true;
+				if (ch == KEY_BACKSPACE and position > 3)
+				{
+					ui->display[23][--position] = ' ';
+					ui->update = true;
+				}
+				else if (ch != KEY_BACKSPACE)
+				{
+					ui->display[23][position++] = ch;
+					ui->update = true;
+				}	
 			}
-			else if (command_start)
+			else
 			{
-				command_start = false;
-				ProcessInput(ui->display[23]);
+				ProcessInput(ui);
 				for (int i=0; i<80; i++)
 					ui->display[23][i] = ' ';
 				ui->update = true;
 				position = 3;
 			}
-			if (ch == ':'){
-				ui->display[23][0] = '-';	ui->display[23][1] = '>';ui->display[23][2] = ' ';
-				command_start = true; ui->update = true;
-			}
 		}
-		usleep(10);
+		usleep(100);
 	}
 	pthread_exit(NULL);
 }
